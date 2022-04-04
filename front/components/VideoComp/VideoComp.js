430451a2a6c2382ff4a0a38199d55017d37e7f4f
Kim-Jin-Uk <gottkfthf@gmail.com>
2022-04-04 17:07:38 +0900

    CREATE - video comp

--- front/components/VideoComp/VideoComp.js
+++ front/components/VideoComp/VideoComp.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './style.module.scss'
 import ReactPlayer from 'react-player/lazy'
 import {string} from "prop-types";
 
-const VideoComp = ({src}) => {
+const VideoComp = () => {
+    const [playIndex, setPlayIndex] = useState(0);
+    const playList = [
+        {index:1, url: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
+        {index:2, url: 'http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8'},
+        {index:3, url: 'http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8'}
+    ];
+    const handleNextVideo = (video, playIndex) => {
+        if(playIndex === video.length - 1){
+            setPlayIndex(0);
+        }else{
+            setPlayIndex(playIndex + 1);
+        }
+    }
 
     return(
         <>
@@ -11,9 +24,16 @@ const VideoComp = ({src}) => {
                 className={styles.video_wrapper}
             >
                 <ReactPlayer
-                    url={src}
-                    playing ={false}
-                    controls
+                    url={playList[playIndex].url}   // 플레이어 url
+                    width='800px'           // 플레이어 크기 (가로)
+                    height='500px'          // 플레이어 크기 (세로)
+                    playing={true}          // 자동 재생 on
+                    muted={true}            // 자동 재생 on
+                    controls={true}         // 플레이어 컨트롤 노출 여부
+                    light={false}           // 플레이어 모드
+                    pip={true}              // pip 모드 설정 여부
+                    poster={'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'}   // 플레이어 초기 포스터 사진
+                    onEnded={() => handleNextVideo(playList, playIndex)}  // 플레이어 끝났을 때 이벤트
                 />
             </div>
         </>
--- front/pages/index.js
+++ front/pages/index.js
@@ -5,7 +5,7 @@ const Home = () => {
     return(
         <>
             <div>2주차</div>
-            <VideoComp src={'https://video.mois.go.kr/upload/video/20220324/VSM_202203241041270370.mp4'}></VideoComp>
+            <VideoComp></VideoComp>
         </>
     )
 }
